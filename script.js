f6cfa8672a0a786bd01456f5c2e6ba8f365883e0
fredburger11 <32545517+fredburger11@users.noreply.github.com>
2017-11-28 23:44:47 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-'use strict';
-
-var haus = document.createElement('h1');
+const haus = document.createElement('h1');
 haus.setAttribute('class', 'h1');
-var text = document.createTextNode('Myndbandaleigan');
+const text = document.createTextNode('Myndbandaleigan');
 haus.appendChild(text);
 document.body.appendChild(haus);
 
-var form;
+const form;
 function makeForm() {
   form = document.createElement('form');
   form.setAttribute('class', 'form');
   document.body.appendChild(form);
 }
 
-var url = 'videos.json';
+const url = 'videos.json';
 
-var request = new XMLHttpRequest();
-var data;
+const request = new XMLHttpRequest();
+const data;
 request.open('GET', url, true);
-request.onload = function () {
+request.onload = function() {
   data = JSON.parse(request.response);
   if (request.status >= 200 && request.status < 400) {
     console.log(data);
-
+    
     showHead(data.categories[0]);
     makeForm();
-
+       
     showData(data.videos[0]);
     showData1(data.videos[1]);
 
@@ -35,122 +33,146 @@ request.onload = function () {
     showData2(data.videos[2]);
     showData3(data.videos[3]);
 
+    
     showHead(data.categories[2]);
     makeForm();
     showData1(data.videos[1]);
     showData3(data.videos[3]);
     showData2(data.videos[2]);
+
   } else {
     console.log('Villa kom upp: ' + data.error);
   }
-};
+}
 
-request.onerror = function () {
+request.onerror = function() {
   console.log('villa');
 };
 
 request.send();
 
+
 function showHead(data) {
 
-  var kall = data.title;
-  var p2 = document.createElement('h1');
+  const kall = data.title;
+  const p2 = document.createElement('h1');
   p2.setAttribute('class', 'h2');
-  var text = document.createTextNode(kall);
+  const text = document.createTextNode(kall);
   p2.appendChild(text);
   document.body.appendChild(p2);
+
 }
 
 function showData(data) {
-
-  var div = document.createElement('div');
+  
+  const div = document.createElement('div');
   div.setAttribute('class', 'kassi');
   form.appendChild(div);
+  
 
-  var img = new Image();
+  const img = new Image();
   img.src = data.poster;
-  var link = 'https://notendur.hi.is/vpv1/stort2/1.html';
+  const link = 'https://notendur.hi.is/vpv1/stort2/1.html';
 
-  var titl = data.title;
+  const titl = data.title;
 
-  var box = document.createElement('a');
+  const box = document.createElement('a');
   box.setAttribute('href', link);
   box.appendChild(img);
   div.appendChild(box);
 
-  var nafn = document.createElement('p');
-  var nafnid = document.createTextNode(titl);
+  const nafn = document.createElement('p');
+  const nafnid = document.createTextNode(titl);
   nafn.appendChild(nafnid);
   div.appendChild(nafn);
+
+
 }
 function showData1(data) {
-
-  var div = document.createElement('div');
+  
+  const div = document.createElement('div');
   div.setAttribute('class', 'kassi');
   form.appendChild(div);
+  
 
-  var img = new Image();
+  const img = new Image();
   img.src = data.poster;
-  var link = 'https://notendur.hi.is/vpv1/stort2/2.html';
+  const link = 'https://notendur.hi.is/vpv1/stort2/2.html';
 
-  var titl = data.title;
+  const titl = data.title;
 
-  var box = document.createElement('a');
+  const box = document.createElement('a');
   box.setAttribute('href', link);
   box.appendChild(img);
   div.appendChild(box);
 
-  var nafn = document.createElement('p');
-  var nafnid = document.createTextNode(titl);
+  const nafn = document.createElement('p');
+  const nafnid = document.createTextNode(titl);
   nafn.appendChild(nafnid);
   div.appendChild(nafn);
+
+
 }
 function showData2(data) {
-
-  var div = document.createElement('div');
+  
+  const div = document.createElement('div');
   div.setAttribute('class', 'kassi');
   form.appendChild(div);
+  
 
-  var img = new Image();
+  const img = new Image();
   img.src = data.poster;
-  var link = 'https://notendur.hi.is/vpv1/stort2/3.html';
+  const link = 'https://notendur.hi.is/vpv1/stort2/3.html';
 
-  var titl = data.title;
+  const titl = data.title;
 
-  var box = document.createElement('a');
+  const box = document.createElement('a');
   box.setAttribute('href', link);
   box.appendChild(img);
   div.appendChild(box);
 
-  var nafn = document.createElement('p');
-  var nafnid = document.createTextNode(titl);
+  const nafn = document.createElement('p');
+  const nafnid = document.createTextNode(titl);
   nafn.appendChild(nafnid);
   div.appendChild(nafn);
+
+
 }
 function showData3(data) {
-
-  var div = document.createElement('div');
+  
+  const div = document.createElement('div');
   div.setAttribute('class', 'kassi');
   form.appendChild(div);
+  
 
-  var img = new Image();
+  const img = new Image();
   img.src = data.poster;
-  var link = 'https://notendur.hi.is/vpv1/stort2/4.html';
+  const link = 'https://notendur.hi.is/vpv1/stort2/4.html';
 
-  var titl = data.title;
+  const titl = data.title;
 
-  var box = document.createElement('a');
+  const box = document.createElement('a');
   box.setAttribute('href', link);
   box.appendChild(img);
   div.appendChild(box);
 
-  var nafn = document.createElement('p');
-  var nafnid = document.createTextNode(titl);
+  const nafn = document.createElement('p');
+  const nafnid = document.createTextNode(titl);
   nafn.appendChild(nafnid);
   div.appendChild(nafn);
+
+
 }
 
-var element = document.querySelector('a');
-element.addEventListener('click', function () {});
 
-//# sourceMappingURL=script-compiled.js.map
+
+const element = document.querySelector('a');
+element.addEventListener('click', function() {
+ 
+});
+
+
+
+
+
+
